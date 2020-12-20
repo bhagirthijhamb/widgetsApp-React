bc218b1cec049cf1b89733cf33b20231c491815f
Bhagirthi Jhamb <bhagirthi.jhamb@gmail.com>
2020-12-20 18:40:04 -0500

    added links to the wikipedia

--- src/components/Search.js
+++ src/components/Search.js
@@ -33,11 +33,13 @@ const Search = () => {
   const renderedResults = results.map(result => {
     return (
       <div key={result.pageid} className="item">
+        <div className="right floated content">
+          <a href={`https://en.wikipedia.org?curid=${result.pageid}`} className="ui button">Go</a>
+        </div>
         <div className="content">
-          <div className="header">
-            {result.title}
-          </div>
-          {result.snippet}
+          <div className="header">{result.title}</div>
+          <span dangerouslySetInnerHTML={{ __html: result.snippet }}></span>
+          {/* {result.snippet} */}
         </div>
       </div>
     )
